4d708d8336f2f28a4a337ad8cd69d2628335af55
Rolf Kristensen <rolf@lightsaber.dk>
2017-10-27 15:38:39 +0200

    added default pw for milage user in image

--- postgres/dockerfile/milage-initdb.d.sh
+++ postgres/dockerfile/milage-initdb.d.sh
@@ -2,7 +2,7 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE USER milage_user;
+    CREATE USER milage_user WITH PASSWORD 'milage_pass';
     CREATE DATABASE milage;
     GRANT ALL PRIVILEGES ON DATABASE milage TO milage_user;
 EOSQL
